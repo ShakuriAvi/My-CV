19c443d59dd8ded615c5b71a71277b7383050b8c
ShakuriAvi <shakuri.avi96@gmail.com>
2021-05-22 20:08:03 +0300

    add project

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -5,6 +5,7 @@ import goalMeet from 'C:/Users/shaku/OneDrive/Desktop/לימודים/React/my-we
 import cardWar from 'C:/Users/shaku/OneDrive/Desktop/לימודים/React/my-web/src/image/cardWarPic.png';
 import FlappyBird from 'C:/Users/shaku/OneDrive/Desktop/לימודים/React/my-web/src/image/flappyBirds.png';
 import myWeb from 'C:/Users/shaku/OneDrive/Desktop/לימודים/React/my-web/src/image/myWeb.png';
+import memoryGameAngular from 'C:/Users/shaku/OneDrive/Desktop/לימודים/React/my-web/src/image/memoryGameAngular.png';
 
 const Project = () => {
     const testData = [
@@ -25,6 +26,9 @@ const Project = () => {
             nameProject: "FlappyBird", tools: "JavaScript / HTML / CSS ", description: " The goal of the game is to fly with the bird and move between the different pipes,\n" +
                 " In the advanced stages of the game, the game will become more difficult.\n When at the end of the game the score of each game appears\n" +
                 "and show option to start a new game.", pictureHref: FlappyBird, href: "https://github.com/ShakuriAvi/FlappyBird"
+        },
+        {
+            nameProject: "Memory Game (Angular)", tools: "Angular / Js / CSS ", description: "Memory game: The player must click in the correct order on the circles after the system which determines the order of pressing the circles randomly.\n Two players participate: the system and the user. Using local storage, animations and more ...", pictureHref: memoryGameAngular, href: "https://github.com/ShakuriAvi/MemoryGame-Angular"
         },
         {
             nameProject: "My Web", tools: "JavaScript / React / CSS ", description: " A site that summarizes my resume, made with React.", pictureHref: myWeb, href: "https://github.com/ShakuriAvi/my-CV"
